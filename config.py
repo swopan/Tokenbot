1a1d1b9df8cba28ccea9e00425341b3bf944d49f
Miko756 <164617880+Miko756@users.noreply.github.com>
2024-05-18 16:21:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,8 +41,6 @@ TUT_VID = os.environ.get("TUT_VID","gojfsi/2")
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
-FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "0"))
-FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "100"))
 
