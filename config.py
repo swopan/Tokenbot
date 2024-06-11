5b01aa498a0ec54a82b43f9f3d47d4bd3746cbbd
Sumitpatel08 <119158472+Sumitpatel08@users.noreply.github.com>
2024-06-11 19:50:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,8 +31,8 @@ PORT = os.environ.get("PORT", "8080")
 DB_URI = "mongodb+srv://sunitverma080:IGjOTga6yh4J0N6V@forcesubs1.uzypzbf.mongodb.net/?retryWrites=true&w=majority&appName=forcesubs1"
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "0")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "0")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "inshorturl.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "d085b925c2b0bafde56a5729a018f45655ad9602")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID","gojfsi/2")
